abbaccc1f099e3607ceb0ec681313b6cbd5783b7
Sujin Kang <1214sujin@naver.com>
2024-11-09 23:05:37 +0900

    :hammer: fix[#10]: OneToMany mappedBy 속성 수정

--- src/main/java/myaong/popolog/blogservice/entity/Comment.java
+++ src/main/java/myaong/popolog/blogservice/entity/Comment.java
@@ -40,7 +40,7 @@ public class Comment extends BaseEntity {
 	@Column(name = "is_blinded", nullable = false)
 	private Boolean isBlinded;
 
-	@OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Comment> replies;
 
 	@Builder
